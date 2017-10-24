7e2309b77ee66c7cbb059247701bf209f834c554
François LETELLIER <f.letellier0@gmail.com>
2017-10-24 16:48:42 +0200

    add variable scss

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -9,9 +9,9 @@
     <link rel="stylesheet" href="/css/app.css"/>
 </head>
 <body>
-<div class=container">
+
     @yield('content')
-</div>
+
 </body>
 <script
         src="https://code.jquery.com/jquery-3.2.1.js"
@@ -22,3 +22,4 @@
 {{--This path avoid the error "Uncaught Error: Bootstrap dropdown require Popper.js"--}}
 <script src="https://getbootstrap.com/assets/js/vendor/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+</html>
