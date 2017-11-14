ccecf6c736359b91d915b6b7ec0f7c50ef6cf216
Johan <johangaub@gmail.com>
2017-11-14 12:03:59 +0100

    Project index WIP

--- resources/views/back/project/index.blade.php
+++ resources/views/back/project/index.blade.php
@@ -13,22 +13,53 @@
         {{--justify-content-between mx-0 px-0--}}
         {{--<div class="col-lg-4 col-md-6 ">--}}
         <form action="{{action('Back\ProjectController@index')}}" method="GET" role="search">
+
             <div class="input-group">
                 <input type="text" class="form-control" name="search" placeholder="Search...">
-                <span class="input-group-btn">
-        <button class="btn btn-default-sm" type="submit"><i class="fa fa-search"></i></button>
-                    </span>
+                <div class="input-group-btn"><button class="btn btn-default-sm" type="submit"><i class="fa fa-search"></i></button>
+                </div>
+            </div>
+
+            <div class="col-auto">
+                <div class="input-group">
+                    <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Search...">
+                    <div class="input-group-addon"><i class="fa fa-search"></i></div>
+                </div>
             </div>
         </form>
+
+
+
+        <div class="row">
+            <div class="col-md-4 col-md-offset-3">
+                <form action="" class="search-form">
+                    <div class="form-group has-feedback">
+                        <label for="search" class="sr-only">Search</label>
+                        <input type="text" class="form-control" name="search" id="search" placeholder="search">
+                        <span class="glyphicon glyphicon-search form-control-feedback"></span>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+
+        <div class="input-group stylish-input-group">
+            <input type="text" class="form-control"  placeholder="Search" >
+            <span class="input-group-addon">
+                        <button type="submit">
+                            <span class="glyphicon glyphicon-search"></span>
+                        </button>
+                    </span>
+        </div>
+
+
+
+
+
     </div>
 
-    <div class="col-auto">
-        <label for="inlineFormInputGroup">Search</label>
-        <div class="input-group mb-2 mb-sm-0">
-            <div class="input-group-search"><i class="fa fa-search"></i></div>
-            <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Username">
-        </div>
-    </div>
+
+
 
 
 
