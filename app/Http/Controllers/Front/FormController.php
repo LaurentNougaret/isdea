93caa98fe5b27b348c17813abeb46ef9f12d4d74
LaurentNougaret <uriel3@hotmail.fr>
2017-12-04 15:15:16 +0100

    WIP of serialize

--- app/Http/Controllers/Front/FormController.php
+++ app/Http/Controllers/Front/FormController.php
@@ -25,28 +25,19 @@ class FormController extends Controller
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function edit($id){
-//		$result = Project::find($id)
-//		                  ->join('results', 'results.project_id', '=','projects.id')
-//		                  ->join('form_project', 'projects.id', '=', 'form_project.project_id')
-//                          ->select('projects.id as project_id', 'results.project_content as content', 'results.id as result_id', 'form_project.form_id as form_id')
-//		                  ->first();
+    public function edit($id)
+    {
         $result = Result::find($id)
             ->join('projects', 'results.project_id', '=', 'projects.id')
             ->join('form_project', 'projects.id', '=', 'form_project.project_id')
             ->select('projects.id as project_id', 'results.project_content as content', 'results.id as result_id', 'form_project.form_id as form_id')
             ->first();
-
         $unserialize = unserialize($result->content);
-
-
         return view('form.form')->with([
             'result' => $result,
             'unserialize'  => $unserialize
         ]);
     }
-
-
 
     /**
      * @param FormUpdateRequest $request
@@ -56,16 +47,9 @@ class FormController extends Controller
      */
     public function update(FormUpdateRequest $request, $result_id)
     {
-        htrhdhtrdhtrd;
         $result = Result::find($result_id);
-//        $result->serialize($request);
-//        $result->update($request);
-//            exit(dump($result));
-htrerhttdyjdt;
-
         return redirect()->route('home', [
             'result' => $result,
-
         ]);
     }
 }
